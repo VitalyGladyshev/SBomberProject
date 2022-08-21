6f0c1a16a8ce9b9912420f344cc3387af05bd8fd
Vitaly Gladyshev <48773035+VitalyGladyshev@users.noreply.github.com>
2022-08-21 21:39:40 +0300

    ДЗ 1 готово

---
+++ SBomberProject/FileLoggerSingletone.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+//Класс функций логирования
+//Шаблон одиночка
+class FileLoggerSingletone
+{
+public:
+    static FileLoggerSingletone& getInstance()
+    {
+        static FileLoggerSingletone instance;
+        return instance;
+    }
+
+    void __fastcall OpenLogFile(const std::string& FN);
+
+    void CloseLogFile();
+
+    void __fastcall WriteToLog(const std::string& str);
+
+    void __fastcall WriteToLog(const std::string& str, int n);
+
+    void __fastcall WriteToLog(const std::string& str, double d);
+
+private:
+    FileLoggerSingletone() {};
+
+    FileLoggerSingletone(const FileLoggerSingletone& root) = delete;
+    FileLoggerSingletone& operator=(const FileLoggerSingletone&) = delete;
+};
+
